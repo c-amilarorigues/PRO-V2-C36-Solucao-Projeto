8815cf38e195c834eea198df99bb0c74dbd5211e
c-amilarorigues <81708586+c-amilarorigues@users.noreply.github.com>
2021-11-22 15:11:32 -0300

    Update Question.js

--- js/Question.js
+++ js/Question.js
@@ -4,7 +4,7 @@ class Question {
     this.title = createElement('h1')
     this.input1 = createInput("Digite Seu Nome Aqui....");
     this.input2 = createInput("Digite o Nº da Opção Correta...");
-    this.button = createButton('Submit');
+    this.button = createButton('Enviar');
     this.question = createElement('h3');
     this.option1 = createElement('h4');
     this.option2 = createElement('h4');
@@ -25,15 +25,15 @@ class Question {
     this.title.html("Meu Jogo de Questionário");
     this.title.position(350, 0);
 
-    this.question.html("Pergunta:- What starts and ends with the letter ‘E’, but has only one letter? " );
+    this.question.html("Pergunta:- O que é que está no meio do começo, no começo do meio, e no final do fim? " );
     this.question.position(150, 80);
-    this.option1.html("1: Everyone " );
+    this.option1.html("1: A letra E " );
     this.option1.position(150, 100);
-    this.option2.html("2: Envelope" );
+    this.option2.html("2: A letra I" );
     this.option2.position(150, 120);
-    this.option3.html("3: Estimate " );
+    this.option3.html("3: A letra M " );
     this.option3.position(150, 140);
-    this.option4.html("4: Example" );
+    this.option4.html("4: A letra O" );
     this.option4.position(150, 160);
 
     this.input1.position(150, 230);
